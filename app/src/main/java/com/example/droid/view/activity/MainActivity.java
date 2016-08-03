e9f8b71a4b980b6765c426637f4e1ea01a100bdc
Songkran Sommit <bsongkran167@gmail.com>
2016-08-03 18:04:58 +0700

    update menu drawer

--- app/src/main/java/com/example/droid/view/activity/MainActivity.java
+++ app/src/main/java/com/example/droid/view/activity/MainActivity.java
@@ -166,9 +166,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
